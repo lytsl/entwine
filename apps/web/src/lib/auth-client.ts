415b046095fc189f5162f9785ad00ed76734c676
lytsl <85685866+lytsl@users.noreply.github.com>
2026-02-01 14:03:29 +0530

    fix: env import

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -1,5 +1,5 @@
 import { createAuthClient } from "better-auth/react";
-import { env } from "env";
+import { env } from "../../env";
 
 export const authClient = createAuthClient({
 	baseURL: env.VITE_SERVER_URL,
--- apps/web/src/utils/api.ts
+++ apps/web/src/utils/api.ts
@@ -1,7 +1,7 @@
 import { hcQuery } from "@entwine/hono-react-query";
 import type { AppType } from "@entwine/server";
-import { env } from "env";
 import { hc } from "hono/client";
+import { env } from "../../env";
 
 const client = hc<AppType>(env.VITE_SERVER_URL, {
 	init: {
